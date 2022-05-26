b7a4b7568f4ec30c9d230f66e92776635f74b042
BabelDev0 <leonardo.straccali@gmail.com>
2022-05-26 15:43:30 +0200

    added: rotation permutatnt

--- GDonut-app/src/components/Calculator.vue
+++ GDonut-app/src/components/Calculator.vue
@@ -1,6 +1,10 @@
 <template>
   <h1>Calculator</h1>
   <div>
+    <div>
+      <input type="text" v-model="polyString" />
+      <button @click="evalPoly()" name="Calcola">Calcola</button>
+    </div>
     <canvas
       id="myCanvasGeneo"
       :width="xTorus"
@@ -19,10 +23,14 @@
     >
       Your browser does not support the HTML5 canvas tag.</canvas
     >
-  </div>
-  <div>
-    <input type="text" v-model="polyString" />
-    <button @click="evalPoly()" name="Calcola">Calcola</button>
+    <canvas
+      id="myCanvas2"
+      :width="xTorus"
+      :height="yTorus"
+      style="border: 1px solid blue"
+    >
+      Your browser does not support the HTML5 canvas tag.</canvas
+    >
   </div>
 </template>
 
@@ -35,6 +43,7 @@ const polyString = ref<string>("");
 
 var myCanvasGeneo: HTMLCanvasElement;
 var myCanvas1: HTMLCanvasElement;
+var myCanvas2: HTMLCanvasElement;
 
 var testImg = new Image();
 var testImgMatrix: Matrix;
@@ -47,6 +56,7 @@ const initTestImage = () => {
   ctx?.clearRect(0, 0, myCanvas1.width, myCanvas1.height);
 
   ctx!.drawImage(testImg, 0, 0);
+
   var testImgData = ctx!.getImageData(0, 0, testImg.width, testImg.height);
   testImgMatrix = useMatrixCanvas().arrayCanvasToMatrix(
     testImgData.data,
@@ -119,11 +129,12 @@ const evalPoly = () => {
 
     var precEl: string;
     poly.forEach((element) => {
-      if (element.includes("a")) {
+      if (element.includes("a1")) {
         let pos = element
-          .replace("a", "")
+          .replace("a1", "")
           .replace(/[\])}[{(]/g, "")
           .split(",");
+
         if (pos.length === 1) {
           pos = ["0", "0"];
         }
@@ -147,20 +158,76 @@ const evalPoly = () => {
         } else {
           torusGeneo = torus_2;
         }
+      } else if (element.includes("a2")) {
+        let degree = element.replace("a2", "").replace(/[\])}[{(]/g, "");
+        if (degree.length === 0) {
+          degree = "0";
+        }
+
+        var ctxRotate = myCanvas2.getContext("2d");
+        ctxRotate?.clearRect(0, 0, myCanvas2.width, myCanvas2.height);
+        ctxRotate?.translate(
+          Math.floor(xTorus.value / 2),
+          Math.floor(yTorus.value / 2)
+        );
+        ctxRotate?.rotate((+degree * Math.PI) / 180);
+        ctxRotate?.translate(
+          -Math.floor(xTorus.value / 2),
+          -Math.floor(yTorus.value / 2)
+        );
+        ctxRotate!.drawImage(testImg, 0, 0);
+
+        var testImgDataRotate = ctxRotate!.getImageData(
+          0,
+          0,
+          testImg.width,
+          testImg.height
+        );
+
+        var torus_2 = useMatrixCanvas().arrayCanvasToMatrix(
+          testImgDataRotate.data,
+          testImg.width,
+          testImg.height
+        );
+
+        ctxRotate?.clearRect(0, 0, myCanvas2.width, myCanvas2.height);
+        ctxRotate?.translate(
+          Math.floor(xTorus.value / 2),
+          Math.floor(yTorus.value / 2)
+        );
+        ctxRotate?.rotate((-degree * Math.PI) / 180);
+        ctxRotate?.translate(
+          -Math.floor(xTorus.value / 2),
+          -Math.floor(yTorus.value / 2)
+        );
+        ctxRotate!.drawImage(testImg, 0, 0);
+
+        if (precEl === "+") {
+          torusGeneo.add(torus_2);
+        } else if (precEl === "-") {
+          torusGeneo.add(torus_2.negate());
+        } else if (precEl === "*") {
+          torusGeneo.mmul(torus_2);
+        } else if (precEl === "/") {
+          torusGeneo.div(torus_2);
+        } else {
+          torusGeneo = torus_2;
+        }
       } else {
         precEl = element;
       }
     });
 
-    useMatrixCanvas().drawMatrix(torusGeneo!, myCanvasGeneo);
+    useMatrixCanvas().drawMatrix(torusGeneo!.div(2), myCanvasGeneo);
   }
 };
 
 onMounted(() => {
   myCanvas1 = <HTMLCanvasElement>document.getElementById("myCanvas1");
+  myCanvas2 = <HTMLCanvasElement>document.getElementById("myCanvas2");
   myCanvasGeneo = <HTMLCanvasElement>document.getElementById("myCanvasGeneo");
 
-  testImg.src = "../../unibo.png";
+  testImg.src = "../../grey.png";
   testImg.onload = () => initTestImage();
 
   xTorus.value = testImg.width;
