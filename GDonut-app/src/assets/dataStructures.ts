2881edb06948334da1478f6ede292a871f876417
BabelDev0 <leonardo.straccali@gmail.com>
2022-12-01 18:15:04 +0100

    fixed: typo

--- GDonut-app/src/assets/dataStructures.ts
+++ GDonut-app/src/assets/dataStructures.ts
@@ -3,25 +3,25 @@ export class DataStructures {
         {
             label: "a_1",
             internalName: "rot",
-            description: "rotations around the centre of the image by 90 degrees",
+            description: "rotations around the center of the image by 90 degrees",
             value: "90",
         },
         {
             label: "a_2",
             internalName: "rot",
-            description: "rotations around the centre of the image by -90 degrees",
+            description: "rotations around the center of the image by -90 degrees",
             value: "-90",
         },
         {
             label: "a_3",
             internalName: "rot",
-            description: "rotations around the centre of the image by 180 degrees",
+            description: "rotations around the center of the image by 180 degrees",
             value: "180",
         },
         {
             label: "a_4",
             internalName: "rot",
-            description: "rotations around the centre of the image by 360 degrees",
+            description: "rotations around the center of the image by 360 degrees",
             value: "360",
         },
         {
@@ -54,14 +54,14 @@ export class DataStructures {
         {
             label: "G1",
             description:
-                "Group formed by rotations around the centre of the image by integer multiples of 90 degrees and a symmetry on the y-axis",
+                "Group formed by rotations around the center of the image by integer multiples of 90 degrees and the symmetry on the y-axis",
             permutants: [this.permutants[0], this.permutants[1], this.permutants[2], this.permutants[3]],
             unknowns: [],
         },
         {
             label: "G2",
             description:
-                "Group formed by rotations around the centre of the image by integer multiples of 90 degrees",
+                "Group formed by rotations around the center of the image by integer multiples of 90 degrees",
             permutants: [this.permutants[4], this.permutants[5], this.permutants[6], this.permutants[7]],
             unknowns: [
                 { label: "x", value: "0" },
